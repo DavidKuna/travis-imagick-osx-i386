fa5f6786d5fb16d7cc1c5a79ba4bbfb3468a8d8e
Travis CI <travis@travis-ci.org>
2019-03-12 12:43:56 +0100

    Update .travis.yml Update push.sh

--- push.sh
+++ push.sh
@@ -6,17 +6,18 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
+commit_staged_files() {
+  ls
   git checkout -b dk-travis
-  git add ./build/*
+  git add git-stage/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/DavidKuna/travis-imagick-osx-i386.git > /dev/null 2>&1
+  git remote add origin https://${GH_USER}:${GH_TOKEN}@github.com/DavidKuna/travis-imagick-osx-i386.git > /dev/null 2>&1
   git push --quiet --set-upstream origin dk-travis 
 }
 
 setup_git
-commit_website_files
+commit_staged_files
 upload_files

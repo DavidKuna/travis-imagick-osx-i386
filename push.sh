30687dcfddf8a0ea0b1dfd0a859decddffc3a789
David Kuna <david.kuna@shopsys.com>
2019-03-12 16:51:01 +0100

    Update configure flags

--- push.sh
+++ push.sh
@@ -10,10 +10,10 @@ commit_staged_files() {
   git remote -v 
   git remote remove origin	
   git remote add origin https://${GH_USER}:${GH_TOKEN}@github.com/DavidKuna/travis-imagick-osx-i386.git > /dev/null 2>&1
-  git checkout -b dk-travis
+  git checkout -b dk-travis-${TRAVIS_BUILD_NUMBER}
   git add git-stage/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git push -u origin dk-travis 
+  git push -u origin dk-travis-${TRAVIS_BUILD_NUMBER}
 }
 
 setup_git
